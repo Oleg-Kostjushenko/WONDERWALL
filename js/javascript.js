35d1216098b59f0fbd51d2cbaa56c5545e882012
Oleg-Kostjushenko <99903642+Oleg-Kostjushenko@users.noreply.github.com>
2024-03-06 23:09:12 +0200

    1

--- js/javascript.js
+++ js/javascript.js
@@ -10,3 +10,13 @@ var swiper = new Swiper(".mySwiper", {
     mousewhell: true,
     keyboard: true,
 })
+
+let menuBtn = document.querySelector('.menu-btn')
+let menu = document.querySelector('.menu')
+
+menuBtn.addEventListener('click', function(){
+    menuBtn.classList.toggle('active');
+    menu.classList.toggle('active')
+    
+})
+
